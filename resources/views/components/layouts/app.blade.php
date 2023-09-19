330ff40f7d280d69dd13ac3b3b925716f536fb76
Spartakos <spartakos.tanidis@chestertons.co.uk>
2023-09-19 17:48:42 +0100

    update README.md

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <div class="min-h-screen bg-gray-100">
             <!-- Page Content -->
             <main>
-                <div class="py-6 mx-auto sm:px-6 lg:px-8">
+                <div class="max-w-5xl py-6 mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900">
                             {{ $slot }}
